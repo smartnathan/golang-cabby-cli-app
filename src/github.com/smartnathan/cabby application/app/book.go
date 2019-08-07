1a0e40c5b11fd0253cade6e36153e25b8715d50f
Nathaniel John <david.nathaniel13@gmail.com>
2019-08-08 00:12:38 +0100

    Refactored code

--- src/github.com/smartnathan/cabby application/app/book.go
+++ src/github.com/smartnathan/cabby application/app/book.go
@@ -11,7 +11,7 @@ import (
 var amountPayable, dropoffPrice, amount int
 
 func main() {
-  var reader = bufio.NewReader(os.Stdin)
+  //var reader = bufio.NewReader(os.Stdin)
   fmt.Println("----------------------------------")
   fmt.Print("|")
   fmt.Print("WELCOME TO CABBY BOOKING PLATFORM")
@@ -20,6 +20,67 @@ func main() {
   fmt.Println("Pickup and dropoff locations: ikono, ikot ekpene, uyo, essien udim, ibesikpo, eket, onna")
   fmt.Println("----------------------------------")
 
+  for {
+    /*Initate the process for pickup and dropoff locations*/
+    getPickupAndDropoff()
+
+    /*Make payment for Delivery*/
+    makeDeliveryPayment()
+
+    /*Request for a tip after delivery is completed*/
+    requestForTip()
+
+  }
+
+}
+
+func requestForTip() {
+  //Get Customer's amount payable
+  fmt.Print("We will appreciate if you give us a tip?-> ")
+  var amount uint16
+  fmt.Scan(&amount)
+
+  if int(amount) <= 0 {
+    fmt.Println("<<<<<<<<<<<<<<<<<<<You are a stingy fool>>>>>>>>>>>>>>>")
+
+  } else if amount < uint16(amountPayable) && amount > 0 {
+    fmt.Println("<<<<<<<<<<<<<<<Thank you very much>>>>>>>>>>>>>>>")
+
+  } else {
+    fmt.Println("<<<<<<<<<<<<<<<<<gracias mucho>>>>>>>>>>>>>>>>")
+
+  }
+}
+
+func makeDeliveryPayment() {
+  count := 0
+  for {
+    //Get Customer's amount payable
+    fmt.Print("Confirm amount for your delivery?-> ")
+    fmt.Scan(&amount)
+    if amount < amountPayable {
+      fmt.Printf("Sorry! The amount is not correct, your cost of delivery is %d\n", amountPayable)
+      count++
+      if count == 5 {
+        fmt.Println("YOU WILL BE REPORTED TO THE POLICE IMMEDIATELY")
+        time.Sleep(2 * time.Second)
+        fmt.Println("Sending notification to the nearest police station....100%")
+        time.Sleep(2 * time.Second)
+        fmt.Println("Police already on their way to your location")
+        break
+      } else if amount >= amountPayable {
+        break
+      }
+    } else if amount > amountPayable {
+      balance := amount - amountPayable
+      fmt.Printf("Thank your patronage, your balance is N%d\n", balance)
+      break
+    }
+  }
+
+}
+
+func getPickupAndDropoff() {
   //Pickup and drop-off points as locations with key as name and value as price
   var locations = map[string]int{
     "ikono":       0,
@@ -31,122 +92,71 @@ func main() {
     "onna":        1600,
   }
 
-  for {
-    //Get customer's pickup point
-    fmt.Print("What is your pickup point?-> ")
-    pickup, _ := reader.ReadString('\n')
-    // convert CRLF to LF
-    pickup = strings.Replace(pickup, "\n", "", -1)
+  reader := bufio.NewReader(os.Stdin)
 
-    //Checking if pickup location does exist
-    pickupPrice, ok := locations[pickup]
+  //Get customer's pickup point
+  fmt.Print("What is your pickup point?-> ")
+  pickup, _ := reader.ReadString('\n')
+  // convert CRLF to LF
+  pickup = strings.Replace(pickup, "\n", "", -1)
 
-    if ok {
-      fmt.Printf("%s is available for pickup\n", pickup)
+  //Checking if pickup location does exist
+  pickupPrice, ok := locations[pickup]
 
-      for {
-        //Get Customer's drop-off point
-        fmt.Print("What is your dropoff point?-> ")
-        dropoff, _ := reader.ReadString('\n')
-        // convert CRLF to LF
-        dropoff = strings.Replace(dropoff, "\n", "", -1)
+  if ok {
+    fmt.Printf("%s is available for pickup\n", pickup)
 
-        //Check if drop off location  is available
-        dropoffPrice, ok = locations[dropoff]
-        if ok {
-          fmt.Printf("%s is available for dropoff\n", dropoff)
-          break
-        } else {
-          continue
-        }
-
-      }
-
-      //Calculate cost for the delivery
-      if dropoffPrice > pickupPrice {
-        amountPayable = dropoffPrice - pickupPrice
-        fmt.Printf("Cost of delivery is N%d\n", amountPayable)
-      } else {
-        amountPayable = pickupPrice - dropoffPrice
-        fmt.Printf("Cost of delivery is N%d\n", amountPayable)
-      }
-
-      //Get Customer's confirmation
-      fmt.Print("Confirm delivery (yes/no)?-> ")
-      confirm, _ := reader.ReadString('\n')
+    for {
+      //Get Customer's drop-off point
+      fmt.Print("What is your dropoff point?-> ")
+      dropoff, _ := reader.ReadString('\n')
       // convert CRLF to LF
-      confirm = strings.Replace(confirm, "\n", "", -1)
-      if strings.Compare("yes", confirm) == 0 {
-        fmt.Println("Processing delivery.................10%")
-        time.Sleep(2 * time.Second)
-        fmt.Println("Processing delivery.................20%")
-        time.Sleep(2 * time.Second)
-        fmt.Println("Processing delivery.................40%")
-        time.Sleep(2 * time.Second)
-        fmt.Println("Processing delivery.................60%")
-        time.Sleep(2 * time.Second)
-        fmt.Println("Processing delivery.................80%")
-        time.Sleep(2 * time.Second)
-        fmt.Println("Delivery Successfully completed.................100%")
+      dropoff = strings.Replace(dropoff, "\n", "", -1)
 
-        count := 0
-        for {
-          tryPayment()
-
-          count++
-          if count == 5 {
-            fmt.Println("YOU WILL BE REPORTED TO THE POLICE IMMEDIATELY")
-            time.Sleep(2 * time.Second)
-            fmt.Println("Sending notification to the nearest police station....90%")
-            time.Sleep(2 * time.Second)
-            fmt.Println("Police already on their way to your location")
-          } else if amount >= amountPayable {
-            break
-          }
-        }
-
-        if amount > amountPayable {
-          balance := amount - amountPayable
-          fmt.Printf("Thank your patronage, your balance is N%d\n", balance)
-        }
-        //Get Customer's amount payable
-        fmt.Print("We will appreciate if you give us a tip?-> ")
-        var amount uint16
-        fmt.Scan(&amount)
-
-        if int(amount) <= 0 {
-          fmt.Println("<<<<<<<<<<<<<<<<<<<You are a stingy fool>>>>>>>>>>>>>>>")
-
-        } else if amount < uint16(amountPayable) && amount > 0 {
-          fmt.Println("<<<<<<<<<<<<<<<Thank you very much>>>>>>>>>>>>>>>")
-
-        } else {
-          fmt.Println("<<<<<<<<<<<<<<<<<gracias mucho>>>>>>>>>>>>>>>>")
-
-        }
-
+      //Check if drop off location  is available
+      dropoffPrice, ok = locations[dropoff]
+      if ok {
+        fmt.Printf("%s is available for dropoff\n", dropoff)
+        break
       } else {
-        fmt.Println("Sorry! We can not proceed with your request. Try again")
+        continue
       }
-
-    } else {
-      fmt.Println("<<<<<<<<<<<<<<<<<Bye, laters>>>>>>>>>>>>>>>")
 
     }
 
-    // if strings.Compare("hi", text) == 0 {
-    //   fmt.Println("hello, Yourself")
-    // }
+    //Calculate cost for the delivery
+    if dropoffPrice > pickupPrice {
+      amountPayable = dropoffPrice - pickupPrice
+      fmt.Printf("Cost of delivery is N%d\n", amountPayable)
+    } else {
+      amountPayable = pickupPrice - dropoffPrice
+      fmt.Printf("Cost of delivery is N%d\n", amountPayable)
+    }
 
+    //Get Customer's confirmation
+    fmt.Print("Confirm delivery (yes/no)?-> ")
+    confirm, _ := reader.ReadString('\n')
+    // convert CRLF to LF
+    confirm = strings.Replace(confirm, "\n", "", -1)
+    if strings.Compare("yes", confirm) == 0 {
+      fmt.Println("Processing delivery.................10%")
+      time.Sleep(2 * time.Second)
+      fmt.Println("Processing delivery.................20%")
+      time.Sleep(2 * time.Second)
+      fmt.Println("Processing delivery.................40%")
+      time.Sleep(2 * time.Second)
+      fmt.Println("Processing delivery.................60%")
+      time.Sleep(2 * time.Second)
+      fmt.Println("Processing delivery.................80%")
+      time.Sleep(2 * time.Second)
+      fmt.Println("Delivery Successfully completed.................100%")
+    } else {
+      fmt.Println("Sorry! We can not proceed with your request. Try again")
+      os.Exit(2)
+    }
+  } else {
+    fmt.Println("<<<<<<<<<<<<<<<<<Bye, laters>>>>>>>>>>>>>>>")
+    os.Exit(2)
   }
-}
 
-func tryPayment() {
-  //Get Customer's amount payable
-  fmt.Print("Confirm amount for your delivery?-> ")
-  fmt.Scan(&amount)
-  if amount < amountPayable {
-    fmt.Printf("Sorry! The amount is not correct, your cost of delivery is %d\n", amountPayable)
-
-  }
 }
